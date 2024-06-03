6555299f60d8678a7ffb12efe72457dc0417ef77
FuryBaM <furybam@gmail.com>
2024-06-03 17:22:55 +0500

    Drone Engine

--- Assets/Scripts/Drone/DroneEngine.cs
+++ Assets/Scripts/Drone/DroneEngine.cs
@@ -1,97 +1,76 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 public class DroneEngine : MonoBehaviour, IEngine 
 {
     [Header("Engine Properties")]
-    [SerializeField] private float _maxPower = 75f; // Максимальная мощность в ваттах
-    [SerializeField] private float _voltage = 12f;
-    [SerializeField] private float _current = 0;
-    [SerializeField] private float _minResistance = 2f;
-    [SerializeField] private float _currentResistance = 100f;
-    [SerializeField] private float _maxResistance = 100f;
-    [SerializeField] private float _propellerRadius = 0.1f; // Радиус пропеллера в метрах
+    [SerializeField] private float _motorKV = 2300f;
+    [SerializeField] private float _batteryVoltage = 11.1f;
+    [SerializeField] private float _batteryCurrent = 6;
+    [SerializeField] private float _propellerRadius = 0.1f; // Propeller radius in meters
     [SerializeField] private bool _isClockwised = true;
     [SerializeField] private Transform _propeller;
 
     [Header("Aerodynamics")]
-    [SerializeField] private float _airDensity = 1.225f; // Плотность воздуха в кг/м^3
+    [SerializeField] private float _airDensity = 1.225f; // Air density in kg/m^3
 
-    [SerializeField] private float _thrust; // Тяга двигателя
+    [SerializeField] private float _thrust; // Engine thrust
     [SerializeField] private float _angularVelocity = 0f;
-    [SerializeField] private float _currentRPM; // Текущие обороты двигателя
-    [SerializeField] private float _currentPower = 0;
+    [SerializeField] private float _currentRPM;
     [SerializeField] private float _torque = 0;
     [SerializeField] private float _airVelocity = 0f;
     [SerializeField] private float _pitch = 0f;
-
-    private void Start()
-    {
-        float maxCurrent = _maxPower / _voltage;
-        _minResistance = _voltage / maxCurrent;
-    }
+    [SerializeField] private float _voltageOutput = 0f;
 
     public void InitEngine() 
     {
-        // Здесь можно добавить логику инициализации двигателя при необходимости
+        // Add any necessary engine initialization logic here
     }
 
     public void UpdateEngine(Rigidbody rigidbody, float throttle) 
     {
-        // Рассчитываем тягу (силу, поднимающую вверх сам винт)
+        // Calculate the thrust (force lifting the propeller)
         CalculateThrust(throttle);
 
-        // Применяем тягу к дрону
+        // Apply the thrust to the drone
         ApplyThrust(rigidbody);
         HandlePropeller(rigidbody);
     }
 
     private void CalculateThrust(float throttle)
     {
+        float voltageInput = _batteryVoltage / 4f;
+        float currentInput = _batteryCurrent / 4f;
+        
+        float propellerEfficiency = 0.85f;
         float motorEfficiency = 0.8f;
+        float controllerEfficiency = 0.95f;
+        float targetVoltageOutput = throttle * voltageInput * controllerEfficiency;
+        _voltageOutput = Mathf.Lerp(_voltageOutput, targetVoltageOutput, Time.deltaTime);
+        float currentPower = _voltageOutput * currentInput * controllerEfficiency;
         
-
-        // Расчет целевого сопротивления на основе дросселя
-        float targetResistance;
-        if (throttle > 0)
-        {
-            float desiredCurrent = throttle * (_maxPower / _voltage);
-            targetResistance = _voltage / desiredCurrent;
-        }
-        else
-        {
-            targetResistance = _maxResistance;
-        }
-
-        _currentResistance = Mathf.Lerp(_currentResistance, targetResistance, Time.deltaTime);
-        
-        // Рассчитываем текущий ток и мощность двигателя
-        _current = _voltage / _currentResistance;
-        _currentPower = Mathf.Min(_voltage * _current, _maxPower);
-
-        // Рассчитываем угловую скорость двигателя (в радианах в секунду)
         float propellerArea = Mathf.PI * Mathf.Pow(_propellerRadius, 2);
-        float powerFactor = motorEfficiency * _currentPower;
-        _airVelocity = motorEfficiency * Mathf.Pow(2 * _currentPower / (Mathf.PI * _airDensity * Mathf.Pow(_propellerRadius*2, 2)*(1-motorEfficiency)), 1f/3f);
-        _angularVelocity = Mathf.Sqrt(2 * powerFactor / (_airDensity * Mathf.PI * Mathf.Pow(_propellerRadius, 2)));
+        _currentRPM = _motorKV * _voltageOutput * motorEfficiency;
+        
+        _angularVelocity = _currentRPM * 2f * Mathf.PI / 60f;
 
-        // Рассчитываем тягу двигателя
-        //_thrust = powerFactor / _voltage;
-        _thrust = powerFactor / _airVelocity;
+        // Correct torque calculation
+        _torque = (currentPower / (_currentRPM / 60f)) * 60f / (2f * Mathf.PI);
 
-        // Рассчитываем крутящий момент
-        _torque = _currentPower / _angularVelocity;
+        // Correct air velocity calculation
+        _airVelocity = Mathf.Pow((currentPower * propellerEfficiency)/(2 * _airDensity * propellerArea), 1f / 3f);
 
-        // Переводим угловую скорость в RPM
-        _currentRPM = _angularVelocity * 60f / (2 * Mathf.PI);
+        // Correct thrust calculation
+        _thrust = 2 * _airDensity * propellerArea * Mathf.Pow(_airVelocity, 2) * propellerEfficiency;
     }
 
     private void ApplyThrust(Rigidbody rigidbody)
     {
-        // Рассчитываем силу тяги, применяемую к дрону
+        // Calculate the thrust force applied to the drone
         Vector3 force = transform.up * _thrust;
 
-        // Применяем силу тяги к центру массы дрона
+        // Apply the thrust force to the center of mass of the drone
         rigidbody.AddForceAtPosition(force, transform.position, ForceMode.Force);
     }
 
@@ -107,9 +86,9 @@ public class DroneEngine : MonoBehaviour, IEngine
             return;
         }
 
-        // Определяем направление вращения пропеллера
-        float rotationDirection = _isClockwised ? 1f : -1f; // 1 - по часовой, -1 - против часовой
+        // Determine the direction of propeller rotation
+        float rotationDirection = _isClockwised ? 1f : -1f; // 1 - clockwise, -1 - counter-clockwise
         _propeller.Rotate(Vector3.up, rotationDirection * _currentRPM * Time.deltaTime);
-        rigidbody.AddTorque(rotationDirection * rigidbody.transform.up * _torque, ForceMode.Force);
+        rigidbody.AddTorque(rotationDirection * transform.up * _torque, ForceMode.Force);
     }
 }
